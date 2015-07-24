02b8be6ef763f094351e097217ce6a383bde295e
Satyajit Sahoo <satyajit.happy@gmail.com>
2015-07-24 20:24:49 +0530

    Fix push notifications

--- app/src/main/java/io/scrollback/neighborhoods/MainActivity.java
+++ app/src/main/java/io/scrollback/neighborhoods/MainActivity.java
@@ -186,18 +186,13 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void onProviderEnabled(String provider) {
-        // TODO Auto-generated method stub
-
     }
 
     @Override
     public void onProviderDisabled(String provider) {
-        // TODO Auto-generated method stub
     }
 }
